53bdd41fc97d4d0dc0c2064c989f4458755100da
Piotr Gondek <piotr.gondek..ext@iceservices.com>
2021-12-23 13:02:47 +0100

    Better UI

--- browser-client/src/App.tsx
+++ browser-client/src/App.tsx
@@ -4,9 +4,17 @@ import "./App.css";
 import { ServerClient } from "./proto/ServerServiceClientPb";
 import { Empty } from "google-protobuf/google/protobuf/empty_pb";
 import { PingRs } from "./proto/server_pb";
-import { ClientReadableStream } from "grpc-web";
+import { ClientReadableStream, RpcError, Status } from "grpc-web";
 
 function App() {
+  const [response, setResponse] = React.useState<PingRs.AsObject | {}>({});
+  const [status, setStatus] = React.useState<Status | void>();
+  const [error, setError] = React.useState<RpcError | void>();
+  const [isFinished, setIsFinished] = React.useState<boolean>(false);
+  const [timestamp, setTimestamp] = React.useState<number>(() =>
+    new Date().getTime()
+  );
+
   React.useEffect(() => {
     const client = new ServerClient("http://localhost:8080");
 
@@ -15,33 +23,49 @@ function App() {
     const stream = client.ping(empty) as ClientReadableStream<PingRs>;
 
     stream.on("data", (response) => {
-      console.log(response.toObject());
+      console.log("Response:", response.toObject());
+      setResponse(response.toObject());
+    });
+
+    stream.on("status", (status) => {
+      console.log({ status });
+      setStatus(status);
     });
 
     stream.on("end", () => {
       console.log("response ended");
+      setIsFinished(true);
     });
 
     stream.on("error", (err) => {
       console.error(err);
+      setError(err);
     });
-  }, []);
+  }, [timestamp]);
+
+  const doAgain = () => {
+    setResponse({});
+    setStatus();
+    setError();
+    setIsFinished(false);
+    setTimestamp(new Date().getTime());
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {isFinished && (
+          <>
+            <h3>Stream is ended!</h3>
+            <button className="button" type="button" onClick={doAgain}>
+              Again!
+            </button>
+          </>
+        )}
+        <pre>Response: {JSON.stringify(response)}</pre>
+        {status && <pre>Status: {JSON.stringify(status)}</pre>}
+        {error && <pre>Error: {JSON.stringify(error.message)}</pre>}
       </header>
     </div>
   );
